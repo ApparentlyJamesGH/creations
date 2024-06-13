402965f23084d703eb308535ed77b1a578588b46
james <140089219+ApparentlyJamesGH@users.noreply.github.com>
2024-06-13 15:09:28 +0300

    Update moon_mod_presets.lua

--- lua/moon_mod_presets.lua
+++ lua/moon_mod_presets.lua
@@ -1,20 +1,8 @@
 return {
   {
     Name = "Print Mod",
-    Files = {
-	{"printButton", "Print"},
-	{"extra", "Extra"}
-	["extra"] = {
-	    {"option1", "Option1"},
-	    "~",
-	    {"option2", "Option2"},
-	}
-    },
     Mod = function(Moon, API)
       API:CreateActionButton("Print", function()
-	print("Hey! This is a Moon Mod test!")
-      end, {})
-      API:CreateActionButton({"extra", {"option1", "~", "option2"}}, function()
 	print("Hey! This is a Moon Mod test!")
       end, {})
     end
