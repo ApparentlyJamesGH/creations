e349ff8525d3034b515d0e2a90d5b89d38d519cb
james <140089219+ApparentlyJamesGH@users.noreply.github.com>
2025-06-23 10:36:16 +0300

    Update moon_mod_presets.lua

--- lua/moon_mod_presets.lua
+++ lua/moon_mod_presets.lua
@@ -6,5 +6,13 @@ return {
 	print("Hey! This is a Moon Mod test!")
       end, {})
     end
-  }
+  },
+  {
+    Name = "Example Mod",
+    Mod = function(Moon, API)
+      API:CreateActionButton("Print", function()
+	print("Hey! This is a Moon Mod test!")
+      end, {})
+    end
+  },
 }
