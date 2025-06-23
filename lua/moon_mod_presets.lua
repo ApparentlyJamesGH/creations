f661a7a459b0f772aac18e793799348a5aaeca34
james <140089219+ApparentlyJamesGH@users.noreply.github.com>
2025-06-23 11:19:34 +0300

    Update moon_mod_presets.lua

--- lua/moon_mod_presets.lua
+++ lua/moon_mod_presets.lua
@@ -1,21 +1,37 @@
+local RunService = game:GetService("RunService")
+
 return {
-  {
-    Name = "Print Mod",
-    Mod = function(Moon, API)
-      API:CreateActionButton("Print", function()
-	print("Hey! This is a Moon Mod test!")
-      end, {})
-    end
-  },
-  {
-    Name = "Autosave",
-    Mod = function(Moon, API)
-      API:CreateActionButton("AutosaveToggle", function(...)
-	Moon.Toggles.CreateToggle("AutosaveToggle", {Default = true})
-	Moon.Toggles.SetToggleChanged("AutosaveToggle", function(value)
-		print(value)				
-	end)
-      end, {"RightAlt"}, {IsToggle = true, NoClickOff = true})
-    end
-  },
+	{
+		Name = "Print Mod",
+		Mod = function(Moon, API)
+			API:CreateActionButton("Print", function()
+				print("Hey! This is a Moon Mod test!")
+			end, {})
+		end
+	},
+	{
+		Name = "Autosave",
+		Mod = function(Moon, API)
+			local animatorWin = Moon.Windows.MoonAnimator
+			local saveConnection, _t = nil, 0
+			
+			API:CreateActionButton("AutosaveToggle", function(...)
+				Moon.Toggles.CreateToggle("AutosaveToggle", {Default = true})
+				Moon.Toggles.SetToggleChanged("AutosaveToggle", function(value)
+					if saveConnection then
+						saveConnection:Disconnect()
+					end
+					
+					if value then
+						saveConnection = RunService.PreRender:Connect(function()
+							if tick() > _t then
+								_t = tick() + 1
+								animatorWin:SaveFile(true)
+							end
+						end)
+					end				
+				end)
+			end, {"ALT_C"}, {IsToggle = true, NoClickOff = true})
+		end
+	},
 }
